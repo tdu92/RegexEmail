1f26b494b27ea301b574c1750e381abb189722ed
=tdu92 <tdu92>
2024-01-20 01:03:17 +0700

    Regex Expression Email input

---
+++ ModelOfEmail.java
@@ -0,0 +1,8 @@
+public class ModelOfEmail {
+    private String model1 = "a@gmail.com";
+    private String model2 = "ab@yahoo.com";
+    private String model3 = "abc@hotmail.com";
+    private String model4 = "@gmail.com";
+    private String model5 = "ab@gmail.";
+    private String model6 = "@#abc@gmail.com";
+}
